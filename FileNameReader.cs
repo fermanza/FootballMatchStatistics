f4b9ec5ca22482e89df2a9495201dc19ef1581de
Fernando Manzano <fernandomanzano@MacBook-Pro-de-Fernando.local>
2024-05-26 17:39:55 -0600

    Modularization Added

--- FileNameReader.cs
+++ FileNameReader.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-class Team
+public interface ITeam
+{
+    string Name { get; }
+    int Goals { get; }
+}
+
+public interface IMatch
+{
+    string TeamName1 { get; }
+    int GoalsTeam1 { get; }
+    int ScoreTeam1 { get; }
+    string TeamName2 { get; }
+    int GoalsTeam2 { get; }
+    int ScoreTeam2 { get; }
+    DateTime GameDate { get; }
+}
+
+public interface IStatistics
+{
+    List<Statistic> Calculate(List<IMatch> matches);
+}
+
+public class Team : ITeam
 {
     public string Name { get; set; }
     public int Goals { get; set; }
 }
 
-class Match
+public class Match : IMatch
 {
     public string TeamName1 { get; set; }
     public int GoalsTeam1 { get; set; }
@@ -20,36 +42,30 @@ class Match
     public DateTime GameDate { get; set; }
 }
 
-class Statistic
+public class Statistic
 {
     public string Name { get; set; }
     public int Points { get; set; }
 }
 
-class FileNameReader
+public class FileReader
 {
-    static void Main()
+    public List<IMatch> ReadMatches(string filePath)
     {
-        string filePath = @"/Users/fernandomanzano/Code/CS/ConsoleApp1/ConsoleApp1/File.txt";
         try
         {
             string[] lines = File.ReadAllLines(filePath);
-            List<Match> matches = lines.Select(line => CreateMatch(line.Split(',')[0].Trim(), line.Split(',')[1].Trim())).ToList();
-            List<Statistic> stats = Stats(matches);
-
-            for (int i = 0; i < stats.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {stats[i].Name}, {stats[i].Points} pts");
-            }
+            return lines.Select(line => CreateMatch(line.Split(',')[0].Trim(), line.Split(',')[1].Trim())).Cast<IMatch>().ToList();
         }
         catch (IOException e)
         {
             Console.WriteLine("The file could not be read:");
             Console.WriteLine(e.Message);
+            return new List<IMatch>();
         }
     }
 
-    static Match CreateMatch(string teamInfo1, string teamInfo2)
+    private IMatch CreateMatch(string teamInfo1, string teamInfo2)
     {
         var teamInfoSubs1 = teamInfo1.Split(' ');
         var teamInfoSubs2 = teamInfo2.Split(' ');
@@ -68,7 +84,7 @@ class FileNameReader
         };
     }
 
-    static Team CreateTeam(string[] teamInfo)
+    private ITeam CreateTeam(string[] teamInfo)
     {
         var team = new Team
         {
@@ -79,9 +95,12 @@ class FileNameReader
         return team;
     }
 
-    static int GetGameScore(int goals1, int goals2) => goals1 > goals2 ? 3 : goals1 == goals2 ? 1 : 0;
+    private int GetGameScore(int goals1, int goals2) => goals1 > goals2 ? 3 : goals1 == goals2 ? 1 : 0;
+}
 
-    static List<Statistic> Stats(List<Match> matches)
+public class StatisticsCalculator : IStatistics
+{
+    public List<Statistic> Calculate(List<IMatch> matches)
     {
         var stats = new List<Statistic>();
 
@@ -94,7 +113,7 @@ class FileNameReader
         return stats;
     }
 
-    static void UpdateStats(List<Statistic> stats, string teamName, int score)
+    private void UpdateStats(List<Statistic> stats, string teamName, int score)
     {
         var existingStat = stats.FirstOrDefault(stat => stat.Name == teamName);
         
@@ -105,6 +124,25 @@ class FileNameReader
         else
         {
             stats.Add(new Statistic { Name = teamName, Points = score });
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string filePath = @"/Users/fernandomanzano/Code/CS/ConsoleApp1/ConsoleApp1/File.txt";
+
+        FileReader fileReader = new FileReader();
+        List<IMatch> matches = fileReader.ReadMatches(filePath);
+
+        IStatistics statisticsCalculator = new StatisticsCalculator();
+        List<Statistic> stats = statisticsCalculator.Calculate(matches);
+
+        for (int i = 0; i < stats.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {stats[i].Name}, {stats[i].Points} pts");
         }
     }
 }
